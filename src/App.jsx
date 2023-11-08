6e6be2e067c22876b7f6d8f3f008e2364eb1f7da
ansina <ansinaup@gmail.com>
2023-11-08 14:19:22 +0530

    basic

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,26 @@ import Home from './Home'
 
 
 function App() {
-  const [state, setState] = useState(5);
 
-  const myfun=(e)=>{
-    console.log('calling');
-    setState(e);
-  }
+  const[color, setColor] =useState()
+  const[state,setState]=useState(0)
+  const increment=()=>setState(state+1);
+  const decrement=()=>state===0 ? ' ' : setState(state-1);
+  const reset=()=>setState(0);
+  // const bgChange=()=>setColor('yellow');
+  const input=(e)=>setColor(e)
+  
 
-  return (
-    <>
-    <Home data={'data'} fun={myfun} call={state}/>  
-    </>
+  
+ 
+
+  return(
+  <>
+  <Home input={input} increment={increment} decrement={decrement} state={state} reset={reset} color={color}/>
+  </>
+
   )
+  
 }
 
 export default App
--- src/Home.jsx
+++ src/Home.jsx
@@ -2,13 +2,33 @@ import { useState } from "react"
 
 function Home (props){
 
-const[count,setCount]=useState(98)
+const increment=()=> props.increment()
+const decrement=()=> props.decrement()
+const reset=()=> props.reset()
+// const bgChange=()=>props.bgChange()
+const input=(e)=>{
+ props.input(e.target.value)
+}
+
+ 
+
     return(
-        <>
-       <h1>{props.data}</h1>
-       <h1>{props.fun(count)}</h1>
-       <h1>{props.call}</h1>
-        </>
+     <>
+     <div className="div" style={{background:props.color}} >
+    <button onClick={increment} >+</button>
+    <h1>{props.state}</h1>
+    {props.state===0 ?
+    <button disabled onClick={decrement}>-</button> :
+    <button onClick={decrement}>-</button>
+
+    }
+
+    <button onClick={reset}>reset</button>
+    {/* <button onClick={bgChange}>bgchange</button> */}
+     <input onChange={input} type="text"></input>
+    </div>
+   
+    </>
     )
 }
 
